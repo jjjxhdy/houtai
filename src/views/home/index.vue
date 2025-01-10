32c666d3932fef5765180bab5c92cb574f6589c1
jjjxhdy <your-email@example.com>
2025-01-10 22:21:42 +0800

    2 commit

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -3,14 +3,7 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue';
-// 获取仓库
-import useUserStore from '@/store/modules/user';
-let userStore = useUserStore()
-// 目前是首页挂载完毕发请求获取用户信息
-onMounted(()=>{
-    userStore.userInfo()
-})
+
 </script>
 
 <style scoped lang="scss"></style>
